f16a26f61d93e1ea2b6f13fc22411ca7e6501cd2
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-06-19 10:08:17 +0200

    Projekt frisch erzeugt und Grundanpassungen gemacht https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.0&packaging=jar&jvmVersion=21&groupId=de.eldecker.spring&artifactId=linkbaum&name=Link-Baum&description=Link-Baum%20(LandingpageI&packageName=de.eldecker.spring.linkbaum&dependencies=data-redis,web,thymeleaf

---
+++ src/main/java/de/eldecker/spring/linkbaum/LinkBaumApplication.java
@@ -0,0 +1,15 @@
+package de.eldecker.spring.linkbaum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LinkBaumApplication {
+
+	public static void main( String[] args ) {
+
+		SpringApplication.run( LinkBaumApplication.class, args );
+	}
+
+}
